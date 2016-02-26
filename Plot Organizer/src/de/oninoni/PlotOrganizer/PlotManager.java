9dd4f45a0ae0fa294b8c571b78c51509a7a004b9
Oninoni <jan@cheap-purple.com>
2016-02-26 16:52:22 +0100

    Comments

--- Plot Organizer/src/de/oninoni/PlotOrganizer/PlotManager.java
+++ Plot Organizer/src/de/oninoni/PlotOrganizer/PlotManager.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+
 public class PlotManager {
 	
 	PlotOrganizer plugin;
@@ -24,10 +26,15 @@ public class PlotManager {
 		
 	}
 	
+	public ProtectedCuboidRegion getNewPlot(Player p){
+		
+	}
+	
 	public void playerEntered(Player p){
 		if(plots.containsKey(p.getPlayer())){
-			
+			// Has a Plot already
 		}else{
+			// Has no Plot
 			
 		}
 	}
