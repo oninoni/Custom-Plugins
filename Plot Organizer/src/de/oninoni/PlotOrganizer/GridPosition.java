942aa40e3264b0617e734dcc9c84687dbaec0b59
Dominik Vogel <xpossseidon@gmail.com>
2016-02-26 16:41:49 +0100

    Added Plot class and Grid Position

---
+++ Plot Organizer/src/de/oninoni/PlotOrganizer/GridPosition.java
@@ -0,0 +1,26 @@
+package de.oninoni.PlotOrganizer;
+
+public class GridPosition {
+	private int x, y;
+	
+	public GridPosition(int x, int y) {
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}	
+}
---
+++ Plot Organizer/src/de/oninoni/PlotOrganizer/Plot.java
@@ -0,0 +1,22 @@
+package de.oninoni.PlotOrganizer;
+
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+
+public class Plot {
+	
+	private GridPosition gridPosition;
+	private ProtectedCuboidRegion protectedCuboidRegion;
+	
+	public Plot(GridPosition gp, PlotOrganizer plugin){
+		this.gridPosition = gp;
+		
+	}
+	
+	public GridPosition getGridPosition() {
+		return gridPosition;
+	}
+	
+	public ProtectedCuboidRegion getProtectedCuboidRegion() {
+		return protectedCuboidRegion;
+	}
+}
