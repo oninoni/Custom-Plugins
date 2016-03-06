6420fb2c692853dad2e09b095d32e58490c90698
Oninoni <jan@cheap-purple.com>
2016-03-06 17:40:20 +0100

    removed Useless

--- OninoniUtil/src/de/oninoni/OninoniUtil/OninoniUtil.java
+++ OninoniUtil/src/de/oninoni/OninoniUtil/OninoniUtil.java
@@ -1,30 +1,14 @@
 package de.oninoni.OninoniUtil;
 
-import java.util.HashMap;
-
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OninoniUtil extends JavaPlugin{
 	
-	private HashMap<OfflinePlayer, Location> homes;
-	
-	private void loadHomes(){
-		homes = new HashMap<OfflinePlayer, Location>();
-		
-		
-	}
-	
-	private void saveHomes(){
-		
-	}
-	
 	public void onEnable() {
-		loadHomes();
+		
 	}
 	
 	public void onDisable() {
-		saveHomes();
+		
 	}
 }
