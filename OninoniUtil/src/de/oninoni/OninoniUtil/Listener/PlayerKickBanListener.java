9033b0987483cb8c03345eac755b4c827cfdfdae
Oninoni <jan@cheap-purple.com>
2016-03-26 22:21:14 +0100

    Jan failed

--- OninoniUtil/src/de/oninoni/OninoniUtil/Listener/PlayerKickBanListener.java
+++ OninoniUtil/src/de/oninoni/OninoniUtil/Listener/PlayerKickBanListener.java
@@ -19,15 +19,16 @@ public class PlayerKickBanListener implements Listener{
 	public void onKick(PlayerKickEvent e){
 		Player player = e.getPlayer();
 		String prefix = plugin.getPermissionsEx().getPermissionsManager().getUser(player).getPrefix();
+		prefix = prefix.replace('&', '§');
 		if(e.getPlayer().isBanned()){
-			plugin.getServer().broadcastMessage(prefix + player.getName() + "&6 has been kicked from the Server!");
+			plugin.getServer().broadcastMessage(prefix + player.getName() + "§6 has been kicked from the Server!");
 			if(e.getReason() != null){
-				plugin.getServer().broadcastMessage("&6because: \"" + e.getReason() + "\"");
+				plugin.getServer().broadcastMessage("§6because: \"" + e.getReason() + "\"");
 			}
 		}else{
-			plugin.getServer().broadcastMessage(prefix + player.getName() + "&6 has been banned from the Server!");
+			plugin.getServer().broadcastMessage(prefix + player.getName() + "§6 has been banned from the Server!");
 			if(e.getReason() != null){
-				plugin.getServer().broadcastMessage("&6because: \"" + e.getReason() + "\"");
+				plugin.getServer().broadcastMessage("§6because: \"" + e.getReason() + "\"");
 			}
 		}
 	}
