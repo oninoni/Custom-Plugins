51eb7a7c148bd529a3b40c37fb5067e8d599fed1
Dominik Vogel <xpossseidon@gmail.com>
2016-04-01 16:19:32 +0200

    Machine Base and Test Class

---
+++ OnionPower/src/de/oninoni/OnionPower/Machines/Generator.java
@@ -0,0 +1,23 @@
+package de.oninoni.OnionPower.Machines;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.util.Vector;
+
+public class Generator extends Machine {
+
+	public Generator(Vector position, World world) {
+		super(position, world);
+	}
+
+	@Override
+	protected boolean isMaterial(Material material) {
+		return material == Material.FURNACE 
+			|| material == Material.BURNING_FURNACE;
+	}
+
+	@Override
+	protected int getMaxEnergy() {
+		return 6400;
+	}
+}
---
+++ OnionPower/src/de/oninoni/OnionPower/Machines/Machine.java
@@ -0,0 +1,29 @@
+package de.oninoni.OnionPower.Machines;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.util.Vector;
+
+public abstract class Machine {
+	
+	private Vector position;
+	private World world;
+	
+	protected int power;
+	
+	public Machine(Vector position, World world){
+		this.position = position;
+		this.world = world;
+	}
+	
+	protected abstract boolean isMaterial(Material material);
+	protected abstract int getMaxEnergy();
+
+	public Vector getPosition() {
+		return position;
+	}
+	
+	public World getWorld() {
+		return world;
+	}
+}
