5ad166848332d75571832ed2ee6b3c6683be0181
Dominik Vogel <xpossseidon@gmail.com>
2016-04-01 17:21:00 +0200

    added MachineManager

---
+++ OnionPower/src/de/oninoni/OnionPower/Machines/MachineManager.java
@@ -0,0 +1,19 @@
+package de.oninoni.OnionPower.Machines;
+
+import java.util.HashMap;
+
+import org.bukkit.util.Vector;
+
+public class MachineManager {
+	
+	private HashMap<Vector, Machine> machines;
+	
+	public MachineManager() {
+		machines = new HashMap<>();
+	}
+	
+	public void Update() {
+		
+	}
+	
+}
--- OnionPower/src/de/oninoni/OnionPower/OnionPower.java
+++ OnionPower/src/de/oninoni/OnionPower/OnionPower.java
@@ -6,11 +6,14 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 
 import de.oninoni.OnionPower.Listeners.InventoryOpenListener;
+import de.oninoni.OnionPower.Machines.MachineManager;
 
 public class OnionPower extends JavaPlugin {
 	
 	private ProtocolManager protocolManager;
 	public ProtocolLibManager protocolLibManager;
+	
+	private MachineManager machineManager;
 	
 	public static OnionPower get(){return JavaPlugin.getPlugin(OnionPower.class);}
 	
@@ -21,6 +24,8 @@ public class OnionPower extends JavaPlugin {
 		
 		InventoryOpenListener inventoryOpenListener = new InventoryOpenListener();
 		getServer().getPluginManager().registerEvents(inventoryOpenListener, this);
+		
+		machineManager = new MachineManager();
 	}
 	
 	public void onDisable() {
