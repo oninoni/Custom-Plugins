4b30342abf8c30e2973e7dc79c082fee27dfcf87
Dominik Vogel <xpossseidon@gmail.com>
2016-04-03 20:17:47 +0200

    CREATION INITIALIZED AND I FOD OG DOOM with lots of fog

---
+++ MiniGames/src/de/oninoni/MiniGames/MiniGames.java
@@ -0,0 +1,22 @@
+package de.oninoni.MiniGames;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MiniGames extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		return false;
+	}
+	
+}
