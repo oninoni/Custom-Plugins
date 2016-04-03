ec801673d21e91882441f751631de82cc2b14e5c
Dominik Vogel <xpossseidon@gmail.com>
2016-04-03 20:49:06 +0200

    more changes

--- MiniGames/src/de/oninoni/MiniGames/Games/TestGame.java
+++ MiniGames/src/de/oninoni/MiniGames/Games/TestGame.java
@@ -1,9 +1,13 @@
 package de.oninoni.MiniGames.Games;
 
+import com.sk89q.worldedit.regions.CuboidRegion;
+
+import de.oninoni.MiniGames.GameManager;
+
 public class TestGame extends Game {
 
-	public TestGame(Lobby lobby, GameManager gameManager) {
-		super(lobby, gameManager);
+	public TestGame(Lobby lobby, GameManager gameManager, CuboidRegion area) {
+		super(lobby, gameManager, area);
 		
 	}
 
