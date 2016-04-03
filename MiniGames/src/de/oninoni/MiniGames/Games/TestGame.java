698a1fd5066f3ff54dcb2bde8976ac22d158c0c5
Oninoni <jan@cheap-purple.com>
2016-04-03 20:49:11 +0200

    To mgerge

--- MiniGames/src/de/oninoni/MiniGames/Games/TestGame.java
+++ MiniGames/src/de/oninoni/MiniGames/Games/TestGame.java
@@ -1,10 +1,11 @@
 package de.oninoni.MiniGames.Games;
 
+import de.oninoni.MiniGames.GameManager;
+
 public class TestGame extends Game {
 
 	public TestGame(Lobby lobby, GameManager gameManager) {
 		super(lobby, gameManager);
-		
 	}
 
 }
