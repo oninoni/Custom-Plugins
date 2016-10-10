d5f58dd3b9d1650b1c8e1980f235833f8aab62c4
Jan <Jan@Onimega_MK2.fritz.box>
2016-10-10 17:08:26 +0200

    Added all Recipes for Tools

--- OnionPower/src/de/oninoni/OnionPower/Items/CraftingRecipes.java
+++ OnionPower/src/de/oninoni/OnionPower/Items/CraftingRecipes.java
@@ -8,6 +8,14 @@ import org.bukkit.inventory.ShapelessRecipe;
 import de.oninoni.OnionPower.OnionPower;
 import de.oninoni.OnionPower.Items.PowerItems.Batrod;
 import de.oninoni.OnionPower.Items.PowerItems.ElectricalAxe;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalBoots;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalChestplate;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalHelmet;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalHoe;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalLeggings;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalPickaxe;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalShovel;
+import de.oninoni.OnionPower.Items.PowerItems.ElectricalSword;
 import de.oninoni.OnionPower.Machines.Upgrades.Upgrade;
 import de.oninoni.OnionPower.Machines.Upgrades.UpgradeManager.UpgradeType;
 
@@ -43,6 +51,62 @@ public class CraftingRecipes {
 		
 		sr.addIngredient(Material.BLAZE_ROD);
 		sr.addIngredient(Material.GOLD_AXE);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalHoe(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_HOE);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalPickaxe(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_PICKAXE);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalShovel(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_SPADE);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalSword(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_SWORD);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalHelmet(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_HELMET);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalChestplate(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_CHESTPLATE);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalLeggings(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_LEGGINGS);
+		
+		Bukkit.addRecipe(sr);
+		
+		sr = new ShapelessRecipe(new ElectricalBoots(0, (short) 0));
+		
+		sr.addIngredient(Material.BLAZE_ROD);
+		sr.addIngredient(Material.GOLD_BOOTS);
 		
 		Bukkit.addRecipe(sr);
 	}
